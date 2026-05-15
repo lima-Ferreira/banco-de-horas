9ea17d87e93e8a1dc705ab43a205fa502e31472d
lima-Ferreira <126613949+lima-Ferreira@users.noreply.github.com>
2026-05-15 15:35:32 -0300

    Filter active employees and add deactivate route
    Updated the search route to only return active employees and added a route to deactivate an employee.

--- backend/routes/funcionarios.js
+++ backend/routes/funcionarios.js
@@ -6,7 +6,9 @@ const checkAdmin = require("../middlewares/checkAdmin"); // 1. Importa o seguran
 // Rota para buscar: QUALQUER UM logado pode ver
 router.get("/", async (req, res) => {
   try {
-    const funcionarios = await Funcionario.find();
+   const funcionarios = await Funcionario.find({
+  ativo: true
+});
     res.json(funcionarios);
   } catch (err) {
     res.status(500).json({ error: "Erro ao buscar funcionários" });
@@ -24,6 +26,26 @@ router.post("/", checkAdmin, async (req, res) => {
   } catch (err) {
     console.error("Erro ao salvar funcionário:", err);
     res.status(500).json({ error: "Erro ao salvar funcionário" });
+  }
+});
+
+// Inativar funcionário
+router.patch("/:id/inativar", checkAdmin, async (req, res) => {
+  try {
+    await Funcionario.findByIdAndUpdate(req.params.id, {
+      ativo: false
+    });
+
+    res.json({
+      success: true,
+      message: "Funcionário inativado"
+    });
+  } catch (err) {
+    console.error("Erro ao inativar funcionário:", err);
+
+    res.status(500).json({
+      error: "Erro ao inativar funcionário"
+    });
   }
 });
 
