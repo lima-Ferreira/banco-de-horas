cc511f38b279780d36c0f3b582e8ed9c7db21d83
lima-Ferreira <126613949+lima-Ferreira@users.noreply.github.com>
2026-05-15 15:53:55 -0300

    Update query to include inactive funcionarios

--- backend/routes/funcionarios.js
+++ backend/routes/funcionarios.js
@@ -6,8 +6,11 @@ const checkAdmin = require("../middlewares/checkAdmin"); // 1. Importa o seguran
 // Rota para buscar: QUALQUER UM logado pode ver
 router.get("/", async (req, res) => {
   try {
-   const funcionarios = await Funcionario.find({
-  ativo: true
+const funcionarios = await Funcionario.find({
+  $or: [
+    { ativo: true },
+    { ativo: { $exists: false } }
+  ]
 });
     res.json(funcionarios);
   } catch (err) {
